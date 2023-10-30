f0b4b31391b97f58ab63e39500185996b50034a7
MatiasColazo1 <maty_colazo@hotmail.com>
2023-10-30 17:46:40 -0300

    actualizacion

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http'
+import { Router } from '@angular/router';
 
 @Injectable({
   providedIn: 'root'
@@ -8,7 +9,7 @@ export class AuthService {
 
   private URL = 'http://localhost:3000/api'
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient, private router: Router) { }
 
   signUp(user: User){
     return this.http.post<User>(this.URL + '/signup', user);
@@ -24,6 +25,11 @@ export class AuthService {
 
   getToken() {
     return localStorage.getItem('token');
+  }
+
+  logOut(){
+    localStorage.removeItem('token')
+    this.router.navigate(['/signin'])
   }
 }
 
