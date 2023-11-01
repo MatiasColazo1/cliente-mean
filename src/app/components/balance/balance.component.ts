db72b035ee59762c7453d2b5e66464a847965e3e
MatiasColazo1 <maty_colazo@hotmail.com>
2023-11-01 16:26:23 -0300

    balance

--- src/app/components/balance/balance.component.ts
+++ src/app/components/balance/balance.component.ts
@@ -33,12 +33,23 @@ export class BalanceComponent {
   };
   public barChartType: ChartType = 'bar';
   public barChartPlugins = [DataLabelsPlugin];
+ 
 
   public barChartData: ChartData<'bar'> = {
-    labels: ['2006', '2007', '2008', '2009', '2010', '2011', '2012'],
+    labels: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'],
     datasets: [
-      { data: [65, 59, 80, 81, 56, 55, 40], label: 'Series A' },
-      { data: [28, 48, 40, 19, 86, 27, 90], label: 'Series B' },
+      { 
+        data: [65, 59, 80, 81, 56, 55, 40], 
+        label: 'Semana pasada',
+        backgroundColor: 'rgba(23, 117, 187, 0.61)', // Color de fondo de la primera barra
+        borderRadius: 10,
+      },
+      { 
+        data: [28, 48, 40, 19, 86, 27, 90], 
+        label: 'Esta semana',
+        backgroundColor: 'rgba(7, 3, 94, 0.8)', // Color de fondo de la segunda barra
+        borderRadius: 10
+      },
     ],
   };
 
@@ -63,19 +74,6 @@ export class BalanceComponent {
     console.log(event, active);
   }
 
-  public randomize(): void {
-    // Only Change 3 values
-    this.barChartData.datasets[0].data = [
-      Math.round(Math.random() * 100),
-      59,
-      80,
-      Math.round(Math.random() * 100),
-      56,
-      Math.round(Math.random() * 100),
-      40,
-    ];
-
-    this.chart?.update();
-  }
+  
 }
 
